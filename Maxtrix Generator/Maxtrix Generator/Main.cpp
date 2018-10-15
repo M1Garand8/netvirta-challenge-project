00109b3e8f4e726c069d054ffdc47b3c611273db
M1Garand8 <m1garand8@hotmail.com>
2018-10-15 23:15:07 +0800

    - Added Matrix Generator project

---
+++ Maxtrix Generator/Maxtrix Generator/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void Main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Format: Matrix Generator.exe <row> <column>\n";
+
+		return;
+	}
+
+
+}
