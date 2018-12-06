7548ce78a7b7bdf311c1018d063fadde20cbbe89
M1Garand8 <m1garand8@hotmail.com>
2018-12-06 23:23:36 +0800

    - Copied EncryptedMatrix, MatrixUtils and StringUtils headers for Matrix Search makefile compilation.

---
+++ Netvirta_Challenge_Project_Sihao/Matrix Search/Header/EncryptedMatrix.h
@@ -0,0 +1,41 @@
+#ifndef ENCRYPTEDMATRIX_H
+#define ENCRYPTEDMATRIX_H
+
+#include "MatrixUtils.h"
+#include <string>
+#include <vector>
+
+class EncryptedMatrix
+{
+public:
+	EncryptedMatrix(unsigned row, unsigned col);
+	EncryptedMatrix(const std::string path, bool encryptDecrypt = true);
+	const unsigned Row() const;
+	const unsigned Col() const;
+	const std::vector<int>& GetMatrixData() const;
+	const std::vector<ElemData>& GetSortedMatrixData() const;
+	const std::vector<ElemData>& GetOldSortedMatrixData() const;
+	const ElemData& operator[](unsigned i) const;
+	const ElemData& GetElemOld(unsigned i) const;
+	const std::vector<int> GetRowData(const unsigned row) const;
+	const std::vector<ElemData> GetSortedRowData(const unsigned row) const;
+	const std::string GetRowString(const unsigned row) const;
+	void GenerateMatrix();
+	void Print();
+	void PrintToFile(const std::string filename, bool encryptDecrypt = true);
+	~EncryptedMatrix();
+private:
+	bool CheckColumnEven();
+	std::string GetRowString(int rowIdx, std::vector<int>& rowData) const;
+	void WriteToFile(std::ofstream& fs, bool encryptDecrypt = true);
+	std::string EncryptDecrypt(std::string toEncrypt);
+	std::vector<ElemData> GenerateSortedRow(const unsigned row);
+	void GenerateSortedMatrix();
+	unsigned _row;
+	unsigned _col;
+	std::vector<int> _matrix;
+	std::vector<ElemData> _matrixSorted;
+	std::vector<ElemData> _matrixSortedOld;
+};
+
+#endif
